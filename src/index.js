566497e1a1df357c0b49d2aeaf4fe4ced9355a78
jabin753 <jabin753@gmail.com>
2018-05-06 23:55:06 -0500

    Morgan Fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var app = require('./config/app')
 var morgan = require('morgan')
-app.use(morgan('dev'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 var server = app.listen(app.get('port'),()=>{
 	console.log('Servidor iniciado en el puerto',app.get('port'))
