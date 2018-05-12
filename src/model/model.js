3d53f1797095ee616dd26b7a9df7613bd2f4a73f
jabin753 <jabin753@gmail.com>
2018-05-12 12:14:29 -0500

    Register paciente throught function

--- src/model/model.js
+++ src/model/model.js
@@ -1,25 +1,10 @@
 var bd = require('../config/dbConnection')
 module.exports = {
 	addUserPaciente: (req,res)=>{
-		const query = {
-			name: 'addUserPaciente',
-			text: 'SELECT adduserpacientes($1,$2,$3,$4,$5,$6,$7,$8,$9)',
-			values: [
-				req.body['Nombre_P'],
-				req.body['A_Paterno_P'],
-				req.body['A_Materno_P'],
-				req.body['Tel_P'],
-				req.body['Email_P'],
-				req.body['Sexo_P'],
-				req.body['F_Nacimiento_P'],
-				req.body['Tipo_Sangre_P'],
-				req.body['Contrasena_P']
-			]	
-		}
-		bd.query(query,(err,result)=>{
+		bd.query({text:'SELECT perfiles.adduserpacientes($1)',
+		values:[req.body]},(err,result)=>{
 			if(!err){
-				res.redirect('/usuario/ingreso')}
-			else console.log(query,err)
+				res.redirect('/usuario/ingreso')} 
 		})
 	}
 }
