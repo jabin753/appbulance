449ac95eb8df1c40466148e3cf861ee297ddc8c7
jabin753 <jabin753@gmail.com>
2018-05-12 12:09:57 -0500

    Morgan removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 var app = require('./config/app')
-var morgan = require('morgan')
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 var server = app.listen(app.get('port'),()=>{
 	console.log('Servidor iniciado en el puerto',app.get('port'))
