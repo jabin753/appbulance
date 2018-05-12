2d5db70d522bedc03ad0c64c6544da8c5deafd60
jabin753 <jabin753@gmail.com>
2018-05-12 12:13:32 -0500

    Style data

--- src/controllers/crum-panel.js
+++ src/controllers/crum-panel.js
@@ -3,24 +3,23 @@ var path = require('path')
 var rootViewPath = 'CRUM Panel'
 module.exports = {
   inicio: (req,res)=>{
-    var reqs = req,res
-    console.log(req.user)
-    res.render(path.join(rootViewPath,'panel-inicio'),{pageTitle:'panel - inicio'})
+    console.log(req.user.style)
+    res.render(path.join(rootViewPath,'panel-inicio'),{pageTitle:'panel - inicio',style:req.user.style})
   },
   peticiones: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-peticiones'),{pageTitle:'panel - peticiones'})
+    res.render(path.join(rootViewPath,'panel-peticiones'),{pageTitle:'panel - peticiones',style:req.user.style})
   },
   tamps: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-tamps'),{pageTitle:'panel - tamps'})
+    res.render(path.join(rootViewPath,'panel-tamps'),{pageTitle:'panel - tamps',style:req.user.style})
   },
   unidades: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-unidades'),{pageTitle:'panel - unidades'})
+    res.render(path.join(rootViewPath,'panel-unidades'),{pageTitle:'panel - unidades',style:req.user.style})
   },
   usuarios: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-usuarios'),{pageTitle:'panel - usuarios atendidos'})
+    res.render(path.join(rootViewPath,'panel-usuarios'),{pageTitle:'panel - usuarios atendidos',style:req.user.style})
   },
   configuracion: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-configuracion'),{pageTitle:'panel - configuracion'})
+    res.render(path.join(rootViewPath,'panel-configuracion'),{pageTitle:'panel - configuracion',style:req.user.style})
   },
   sesion: (req,res,next) =>{
     if (req.isAuthenticated()) { 
