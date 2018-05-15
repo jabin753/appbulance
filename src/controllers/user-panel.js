b82d74de54a3269ca8a94e9b0b549eccb50fea95
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-05-15 14:31:43 -0500

    Creacion de controlador del usuario

---
+++ src/controllers/user-panel.js
@@ -0,0 +1,19 @@
+
+var path = require('path')
+var rootViewPath = 'User panel'
+module.exports = {
+  inicio: (req,res)=>{
+    console.log(req.user.style)
+    res.render(path.join(rootViewPath,'panel-inicio'),{pageTitle:'panel - inicio',style:req.user.style})
+  },
+  historial_peticiones: (req,res)=>{
+    res.render(path.join(rootViewPath,'panel-peticiones'),{pageTitle:'panel - peticiones',style:req.user.style})
+  },
+  sesion: (req,res,next) =>{
+    if (req.isAuthenticated()) { 
+      next()
+    } else {
+      res.redirect('/usuario/ingreso');
+    }
+  }
+}
