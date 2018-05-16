fe4f640518b193d9b660d2d9abeed27bc8585591
jabin753 <jabin753@gmail.com>
2018-05-15 21:20:18 -0500

    crum passport added

--- src/config/passport.js
+++ src/config/passport.js
@@ -21,6 +21,33 @@ module.exports = (passport)=>{
                 console.log(err,result)
                 if(!err){
                     if(result.rows[0]['authuser']){
+                        var user = {id_usr:result.rows[0]['authuser'],
+                        style:{
+                            nav_style:'navbar-light',
+                            nav_background_style:'bg-light',
+                            body_style:'bg-light' 
+                        }}
+                        return done(null, user)
+                    }
+                    else return done(null, false)
+                }
+                else return done(null, false)
+            }
+            
+        ) 
+    }))
+
+    //Autenticación local para el CRUM
+    passport.use('local-crum',new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'contra',
+        passReqToCallback: true},  
+        (req,email,contra,done) => {
+            db.query({text:'SELECT perfiles.authuser($1)',
+            values:[req.body]},(err,result)=>{
+                console.log(err,result)
+                if(!err){
+                    if(result.rows[0]['authuser']){
                         var user = {id_p:result.rows[0]['authuser'],
                         style:{
                             nav_style:'navbar-light',
