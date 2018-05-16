9f218ccd231e62b62a3e27d28b517cd39064e13d
jabin753 <jabin753@gmail.com>
2018-05-15 23:22:29 -0500

    Routes Fixed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,47 @@
 var inicio = require('../controllers/inicio')
-var panel  = require('../controllers/crum-panel')
+var crum  = require('../controllers/crum-panel')
 var user  = require('../controllers/user-panel')
 var login  = require('../controllers/login')
 var registration  = require('../controllers/registration')
 module.exports = (app,passport) => {    
     app.get('/',inicio.inicio)
-    app.get('/usuario/inicio', )
+    
+    //Exclusive Users Routes
+    app.get('/usuario/*',user.sesion)
     app.get('/usuario/registro',registration.userPaciente)
     app.post('/usuario/registro',registration.addUserPaciente)
-    
-    app.get('/usuario/ingreso',login.sesion,login.user)
+    app.get('/usuario/ingreso',login.validateUser,login.user)
     app.post('/usuario/ingreso',passport.authenticate('local-usuario',{
         successRedirect : '/usuario/inicio',
         failureRedirect : '/usuario/ingreso'
     }))
     app.get('/usuario/inicio',user.sesion,user.inicio)
-    app.get('/crum',panel.sesion,panel.inicio)
-    app.get('/crum/*',panel.sesion)
-    app.get('/crum/peticiones',panel.peticiones)
-    app.get('/crum/tamps',panel.tamps)
-    app.get('/crum/unidades',panel.unidades)
-    app.get('/crum/usuarios',panel.usuarios)
-    app.get('/crum/configuracion',panel.configuracion)
+<<<<<<< HEAD
+
+    //Exclusive CRUM Routes
+    app.get('/crum/registro',registration.userCrum)
+    app.post('/crum/registro',registration.addUserCrum)
+    app.get('/crum/ingreso',login.validateCrum,login.crum)
+    app.post('/crum/ingreso',passport.authenticate('local-crum',{
+        successRedirect : '/crum/inicio',
+        failureRedirect : '/crum/ingreso'
+    }))
+    app.get('/crum/*',crum.sesion)
+    app.get('/crum/inicio',crum.inicio)
+=======
+    app.get('/crum',crum.sesion,crum.inicio)
+    app.get('/crum/*',crum.sesion)
+>>>>>>> 14f5f3632899ea2af3b7541818478f633a3caaf8
+    app.get('/crum/peticiones',crum.peticiones)
+    app.get('/crum/tamps',crum.tamps)
+    app.get('/crum/unidades',crum.unidades)
+    app.get('/crum/usuarios',crum.usuarios)
+    app.get('/crum/configuracion',crum.configuracion)
+<<<<<<< HEAD
     
+=======
+    app.get('/crum/registro')   /*AQUI AGREGA LOS PARAMETROS DE SESION*/
+>>>>>>> 14f5f3632899ea2af3b7541818478f633a3caaf8
     app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/usuario/ingreso');
