7304e3494397641f2bbb5be1a6967a3c3c772d7d
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-05-16 20:15:46 -0500

    Modifciacion en el titulo de la pestaña

--- src/controllers/crum-panel.js
+++ src/controllers/crum-panel.js
@@ -4,22 +4,22 @@ var rootViewPath = 'CRUM Panel'
 module.exports = {
   inicio: (req,res)=>{
     console.log(req.user.style)
-    res.render(path.join(rootViewPath,'panel-inicio'),{pageTitle:'panel - inicio',style:req.user.style})
+    res.render(path.join(rootViewPath,'panel-inicio'),{pageTitle:'Appbulance (CRUM) - Inicio',style:req.user.style})
   },
   peticiones: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-peticiones'),{pageTitle:'panel - peticiones',style:req.user.style})
+    res.render(path.join(rootViewPath,'panel-peticiones'),{pageTitle:'Appbulance (CRUM) - Peticiones',style:req.user.style})
   },
   tamps: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-tamps'),{pageTitle:'panel - tamps',style:req.user.style})
+    res.render(path.join(rootViewPath,'panel-tamps'),{pageTitle:'Appbulance (CRUM) - TAMPS',style:req.user.style})
   },
   unidades: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-unidades'),{pageTitle:'panel - unidades',style:req.user.style})
+    res.render(path.join(rootViewPath,'panel-unidades'),{pageTitle:'Appbulance (CRUM) - Unidades',style:req.user.style})
   },
   usuarios: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-usuarios'),{pageTitle:'panel - usuarios atendidos',style:req.user.style})
+    res.render(path.join(rootViewPath,'panel-usuarios'),{pageTitle:'Appbulance (CRUM) - Usuarios atendidos',style:req.user.style})
   },
   configuracion: (req,res)=>{
-    res.render(path.join(rootViewPath,'panel-configuracion'),{pageTitle:'panel - configuracion',style:req.user.style})
+    res.render(path.join(rootViewPath,'panel-configuracion'),{pageTitle:'Appbulance (CRUM) - Configuracion',style:req.user.style})
   },
   sesion: (req,res,next) =>{
     if (req.isAuthenticated()) { 
