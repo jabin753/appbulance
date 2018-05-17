94ef12124beec265ddf201f1c5dea4c4940d4c76
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-05-16 23:26:52 -0500

    Corregir errores

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,7 +15,6 @@ module.exports = (app,passport) => {
         failureRedirect : '/usuario/ingreso'
     }))
     app.get('/usuario/inicio',user.sesion,user.inicio)
-    app.get('/tamp/inicio',user.sesion,user.tamp) //Ruta del TAMP tempora y provisional para pruebas. Tiene parametros de usuario normal.
 
     //Exclusive CRUM Routes
     app.get('/crum/registro',registration.userCrum)
