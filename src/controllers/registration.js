926b7d3af633490064fedf2e19baf50e3fc4d3af
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-05-16 20:17:40 -0500

    Modificacion del titulo de la pestaña (title)

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,10 +1,10 @@
 var consultas = require('../model/model')
 module.exports = {
   userCrum: (req,res)=>{
-    res.render('crum-registration',{pageTitle:'appbulance - registro'})
+    res.render('crum-registration',{pageTitle:'Appbulance (CRUM) - Registro'})
   },
   userPaciente: (req,res)=>{
-    res.render('user-registration',{pageTitle:'appbulance - registro'})
+    res.render('user-registration',{pageTitle:'Appbulance - Registro'})
   },
   addUserPaciente: consultas.addUserPaciente,
   addUserCrum: (req,res,next)=>{
