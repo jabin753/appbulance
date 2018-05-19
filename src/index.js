31ab2cfba20a82818f54ff875be4f33a896ac5da
jabin753 <jabin753@gmail.com>
2018-05-18 22:24:29 -0500

    new run log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 var app = require('./config/app')
 
 var server = app.listen(app.get('port'),()=>{
-	console.log('Servidor iniciado en el puerto',app.get('port'))
+	var host = server.address().address
+    var port = server.address().port
+	console.log('Servidor iniciado en %s:%s',host,port)
+	
 })
 
 
