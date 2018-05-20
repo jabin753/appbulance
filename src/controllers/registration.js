b24c2a150bb39dafbc75486f260dc685f952450e
jabin753 <jabin753@gmail.com>
2018-05-19 19:48:05 -0500

    User registration Flash messages implemented

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,13 +1,13 @@
 var consultas = require('../model/model')
 module.exports = {
   userCrum: (req,res)=>{
-    res.render('crum-registration',{pageTitle:'Appbulance (CRUM) - Registro'})
+    res.render('crum-registration',{pageTitle:'Appbulance (CRUM) - Registro',flashMessage: req.flash('crum-registration')})
   },
   userPaciente: (req,res)=>{
-    res.render('user-registration',{pageTitle:'Appbulance - Registro'})
+    res.render('user-registration',{pageTitle:'Appbulance - Registro',flashMessage: req.flash('user-registration')})
   },
   userTamp: (req,res)=>{
-    res.render('tamp-registration',{pageTitle:'Appbulance - Registro'})
+    res.render('tamp-registration',{pageTitle:'Appbulance - Registro',flashMessage: req.flash('tamp-registration')})
   },
   addUserCrum: consultas.addUserCrum,
   addUserPaciente: consultas.addUserPaciente,
