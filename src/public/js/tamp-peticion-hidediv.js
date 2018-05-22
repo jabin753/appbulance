04fcdd16140e728bb4e007925efbd5763c895747
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-05-22 10:02:57 -0500

    Archivos del TAMP

---
+++ src/public/js/tamp-peticion-hidediv.js
@@ -0,0 +1,33 @@
+/*Peticion*/
+function hideDivI(){
+  $("#divPeticion").hide();
+  $("#btnDownI").show();
+  $("#btnUpI").hide();
+}
+function showDivI(){
+  $("#divPeticion").show();      
+  $("#btnDownI").hide();
+  $("#btnUpI").show();
+}
+/*II. Datos de servicio*/
+function hideDivII(){
+  $("#divServicio").hide();
+  $("#btnDownII").show();
+  $("#btnUpII").hide();
+}
+function showDivII(){
+  $("#divServicio").show();      
+  $("#btnDownII").hide();
+  $("#btnUpII").show();
+}
+/*III. Control*/
+function hideDivIII(){
+  $("#divControl").hide();
+  $("#btnDownIII").show();
+  $("#btnUpIII").hide();
+}
+function showDivIII(){
+  $("#divControl").show();      
+  $("#btnDownIII").hide();
+  $("#btnUpIII").show();
+}
