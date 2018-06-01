ab218102fd156e89ae4c280bdfc8cca703d4bbbb
jabin753 <jabin753@gmail.com>
2018-06-01 09:20:21 -0500

    Crum methods added

--- src/model/model.js
+++ src/model/model.js
@@ -74,27 +74,164 @@ module.exports = {
 			}
 		})
 	},
-	crumPanelTamp: (req, res) => {},
+
+	crumPanelPeticiones: (req,res)=>{
+		bd.query({
+			text: 'SELECT * FROM peticiones.peticiones WHERE peticiones.id_cm = $1',
+			values: [req.user.id_cm]
+		}, (err,result) => {
+			console.log(err,result.rows)
+			res.render('CRUM Panel/panel-peticiones',
+			{pageTitle:'Appbulance (CRUM) - Peticiones',
+			style:req.user.style,
+			data:result.rows})
+		})
+		
+	},
+	crumPanelPeticiones_A: (req,res)=>{
+		bd.query({
+			text: 'INSERT ...',
+			values: [req.body['id_pt'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+	crumPanelPeticiones_B: (req,res)=>{
+		bd.query({
+			text: 'DELETE ...',
+			values: [req.body['id_pt'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+	crumPanelPeticiones_C: (req,res)=>{
+		bd.query({
+			text: 'UPDATE ...',
+			values: [req.body['id_pt'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+
+	crumPanelTamps: (req,res)=>{
+		bd.query({
+			text: 'SELECT * FROM perfiles.tamps WHERE tamps.id_cm = $1',
+			values: [req.user.id_cm]
+		}, (err,result) => {
+			console.log(err,result.rows)
+			res.render('CRUM Panel/panel-peticiones',{
+			pageTitle:'Appbulance (CRUM) - Peticiones',
+			style:req.user.style,
+			data:result.rows})
+		})
+	},
+	crumPanelTamps_A: (req,res)=>{
+		bd.query({
+			text: 'INSERT ...',
+			values: [req.body['id_tmp'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+	crumPanelTamps_B: (req,res)=>{
+		bd.query({
+			text: 'DELETE ...',
+			values: [req.body['id_tmp'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+	crumPanelTamps_C: (req,res)=>{
+		bd.query({
+			text: 'UPDATE ...',
+			values: [req.body['id_tmp'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+
 	crumPanelUnidades: (req, res) => {
 		bd.query({
-			text: 'SELECT * FROM administracion.get_a($1)',
-			values: [req.user.id_usr]
-		}, (err, response) => {
+			text: 'SELECT * FROM administracion.ambulancias WHERE ambulancias.id_cm = $1',
+			values: [req.user.id_cm]
+		}, (err, result) => {
+			console.log(err,result.rows)
 			res.render('CRUM Panel/panel-unidades', {
 				pageTitle: 'Appbulance (CRUM) - Unidades Médicas',
 				style: req.user.style,
-				data: response.rows
+				data: result.rows
 			})
 		})
 	},
-	crumPanelUnidadesAdd: (req, res) => {
+	crumPanelUnidades_A: (req, res) => {
 		bd.query({
 			text: 'INSERT INTO administracion.ambulancias(num_placa_a, num_economico_a, id_cm, estado_a)' +
-				'VALUES($1,$2,perfiles.get_cm_id($3),$4)',
-			values: [req.body['num_placa_a'], req.body['num_economico_a'], req.user.id_usr, req.body['estado_a']]
-		}, (err, response) => {
-			console.log(err, response);
+				'VALUES($1,$2,$3,$4)',
+			values: [req.body['num_placa_a'], req.body['num_economico_a'], req.user.id_cm, req.body['estado_a']]
+		}, (err, result) => {
+			console.log(err, result);
 			res.redirect('/crum/unidades')
+		})
+	},
+	crumPanelUnidades_B: (req,res) => {
+		console.log(req.body)
+		bd.query({
+			text: 'DELETE FROM administracion.ambulancias WHERE ambulancias.num_placa_a = $1 AND ambulancias.id_cm = $2',
+			values: [req.body['num_placa_a'],req.user.id_cm]
+		}, (err,result) => {
+			console.log(err, result);
+			res.redirect('/crum/unidades')
+		})
+	},
+	crumPanelUnidades_C: (req,res) => {
+		console.log(req.body)
+		bd.query({
+			text: 'UPDATE administracion.ambulancias SET '+
+				'num_placa_a = $2, num_economico_a=$3, estado_a=$4 WHERE id_a=$1 AND id_cm=$5',
+			values: [req.body['id_a'],req.body['num_placa_a'],req.body['num_economico_a'],req.body['estado_a'],req.user.id_cm]
+		}, (err,result) => {
+			console.log(err, result);
+				res.redirect('/crum/unidades')
+		})
+	},
+
+	crumPanelUsuarios: (req, res) => {
+		bd.query({
+			text: 'SELECT * FROM perfiles.pacientes WHERE id_cm = $1',
+			values: [req.user.id_cm]
+		}, (err, result) => {
+			console.log(err,result.rows)
+			res.render('CRUM Panel/panel-usuarios', {
+				pageTitle: 'Appbulance (CRUM) - Unidades Médicas',
+				style: req.user.style,
+				data: result.rows
+			})
+		})
+	},
+	crumPanelUsuarios_A: (req, res) => {
+		bd.query({
+			text: 'INSERT ...',
+			values: [req.body['id_usr'],req.user.id_cm]
+		}, (err, result) => {
+			console.log(err, result);
+			res.redirect('/crum/usuarios')
+		})
+	},
+	crumPanelUsuarios_B: (req,res) => {
+		bd.query({
+			text: 'DELETE FROM ...',
+			values: [req.body['id_usr'],req.user.id_cm]
+		}, (err,result) => {
+
+		})
+	},
+	crumPanelUsuarios_C: (req,res) => {
+		bd.query({
+			text: 'UPDATE ...',
+			values: [req.body['id_usr'],req.user.id_cm]
+		}, (err,result) => {
+
 		})
 	}
 }
+
