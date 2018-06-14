3b6a69db8312328e5661e3f679c1f86b5db91102
jabin753 <jabin753@gmail.com>
2018-06-13 22:52:31 -0500

    CRUM CRUD methods asynced

--- src/model/model.js
+++ src/model/model.js
@@ -40,41 +40,35 @@ module.exports = {
 			}
 		})
 	},
-	authuser: (req, email, contra, done) => {
-		bd.query({
-			text: 'SELECT  * FROM perfiles.authuser($1)',
-			values: [req.body]
-		}, (err, result) => {
-			if (!err) {
-				if (result.rowCount > 0) {
-					var user = {
-						id_usr: result.rows[0]['id_usr'],
-						tipo_usr: result.rows[0]['tipo_usr'],
-						style: {
-							nav_style: result.rows[0]['nav_style'],
-							body_style: result.rows[0]['body_style']
-						}
-					}
-					if (user.tipo_usr === 1) {
-						user['id_cm'] = result.rows[0]['id']
-						return done(null, user)
-					} else if (user.tipo_usr === 2) {
-						user['id_p'] = result.rows[0]['id']
-						return done(null, user)
-					} else if (user.tipo_usr === 3) {
-						user['id_tmp'] = result.rows[0]['id']
-						return done(null, user)
-					}
-					return done(null, false)
+	authuser: async (req, email, contra, done) => {
+		try{
+			const res = await bd.query('SELECT  * FROM perfiles.authuser($1)',[req.body])
+			var user = {
+				id_usr: res.rows[0]['id_usr'],
+				tipo_usr: res.rows[0]['tipo_usr'],
+				style: {
+					nav_style: res.rows[0]['nav_style'],
+					body_style: res.rows[0]['body_style']
 				}
-
-			} else {
-				req.flash('user-login', err.hint)
-				return done(null, null)
 			}
-		})
+			switch(user.tipo_usr){
+				case 1:
+					user['id_cm'] = res.rows[0]['id']
+					return done(null, user)
+				case 2:
+					user['id_p'] = res.rows[0]['id']
+					return done(null, user)
+				case 3:
+					user['id_tmp'] = res.rows[0]['id']
+					return done(null, user)
+				default:
+				return done(null, false)
+			}
+		}catch(err){
+			req.flash('user-login', err.hint)
+			return done(null, false)
+		}
 	},
-
 	crumPanelPeticiones: (req,res)=>{
 		bd.query({
 			text: 'SELECT * FROM peticiones.peticiones WHERE peticiones.id_cm = $1',
@@ -93,7 +87,7 @@ module.exports = {
 			text: 'INSERT ...',
 			values: [req.body['id_pt'],req.user.id_cm]
 		}, (err,result) => {
-
+			
 		})
 	},
 	crumPanelPeticiones_B: (req,res)=>{
@@ -101,7 +95,7 @@ module.exports = {
 			text: 'DELETE ...',
 			values: [req.body['id_pt'],req.user.id_cm]
 		}, (err,result) => {
-
+			
 		})
 	},
 	crumPanelPeticiones_C: (req,res)=>{
@@ -109,10 +103,9 @@ module.exports = {
 			text: 'UPDATE ...',
 			values: [req.body['id_pt'],req.user.id_cm]
 		}, (err,result) => {
-
+			
 		})
 	},
-
 	crumPanelTamps: (req,res)=>{
 		bd.query({
 			text: 'SELECT * FROM perfiles.tamps WHERE tamps.id_cm = $1',
@@ -120,25 +113,24 @@ module.exports = {
 		}, (err,result) => {
 			console.log(err,result.rows)
 			res.render('CRUM Panel/panel-tamps',{
-			pageTitle:'Appbulance (CRUM) - tamps',
-			style:req.user.style,
-			data:result.rows})
-		})
+				pageTitle:'Appbulance (CRUM) - tamps',
+				style:req.user.style,
+				data:result.rows})
+			})
 	},
 	crumPanelTamps_A: (req,res)=>{
 		bd.query({
 			text: 'INSERT ...',
 			values: [req.body['id_tmp'],req.user.id_cm]
-		}, (err,result) => {
-
-		})
+		}, (err,result) => {		
+			})
 	},
 	crumPanelTamps_B: (req,res)=>{
 		bd.query({
 			text: 'DELETE ...',
 			values: [req.body['id_tmp'],req.user.id_cm]
 		}, (err,result) => {
-
+				
 		})
 	},
 	crumPanelTamps_C: (req,res)=>{
@@ -146,55 +138,65 @@ module.exports = {
 			text: 'UPDATE ...',
 			values: [req.body['id_tmp'],req.user.id_cm]
 		}, (err,result) => {
-
-		})
-	},
-
-	crumPanelUnidades: (req, res) => {
-		bd.query({
-			text: 'SELECT * FROM administracion.ambulancias WHERE ambulancias.id_cm = $1',
-			values: [req.user.id_cm]
-		}, (err, result) => {
-			console.log(err,result.rows)
+				
+			})
+	},	
+	crumPanelUnidades: async(req, res) => {
+		try{
+			const result = await bd.query(
+				'SELECT * FROM administracion.ambulancias WHERE ambulancias.id_cm = $1',
+				[req.user.id_cm]
+			)
 			res.render('CRUM Panel/panel-unidades', {
 				pageTitle: 'Appbulance (CRUM) - Unidades Médicas',
 				style: req.user.style,
 				data: result.rows
 			})
-		})
+		}
+		catch(err){
+			console.log(err)
+		}		
 	},
-	crumPanelUnidades_A: (req, res) => {
-		bd.query({
-			text: 'INSERT INTO administracion.ambulancias(num_placa_a, num_economico_a, id_cm, estado_a)' +
+	crumPanelUnidades_A: async(req, res) => {
+		try{
+			const result = await bd.query(
+				'INSERT INTO administracion.ambulancias(num_placa_a, num_economico_a, id_cm, estado_a)' +
 				'VALUES($1,$2,$3,$4)',
-			values: [req.body['num_placa_a'], req.body['num_economico_a'], req.user.id_cm, req.body['estado_a']]
-		}, (err, result) => {
-			console.log(err, result);
+				[req.body['num_placa_a'], req.body['num_economico_a'], req.user.id_cm, req.body['estado_a']]	
+			)
 			res.redirect('/crum/unidades')
-		})
+		}
+		catch(err){
+			console.log(err)
+		}
 	},
-	crumPanelUnidades_B: (req,res) => {
-		console.log(req.body)
-		bd.query({
-			text: 'DELETE FROM administracion.ambulancias WHERE ambulancias.num_placa_a = $1 AND ambulancias.id_cm = $2',
-			values: [req.body['num_placa_a'],req.user.id_cm]
-		}, (err,result) => {
-			console.log(err, result);
+	crumPanelUnidades_B: async(req,res) => {
+		try{
+			const result = await bd.query(
+				'DELETE FROM administracion.ambulancias '+
+				'WHERE ambulancias.num_placa_a = $1 AND ambulancias.id_cm = $2',
+				[req.body['num_placa_a'],req.user.id_cm]	
+			)
 			res.redirect('/crum/unidades')
-		})
+		}
+		catch(err){
+			console.log(err)
+		}
 	},
-	crumPanelUnidades_C: (req,res) => {
-		console.log(req.body)
-		bd.query({
-			text: 'UPDATE administracion.ambulancias SET '+
-				'num_placa_a = $2, num_economico_a=$3, estado_a=$4 WHERE id_a=$1 AND id_cm=$5',
-			values: [req.body['id_a'],req.body['num_placa_a'],req.body['num_economico_a'],req.body['estado_a'],req.user.id_cm]
-		}, (err,result) => {
-			console.log(err, result);
-				res.redirect('/crum/unidades')
-		})
-	},
-
+	crumPanelUnidades_C: async(req,res) => {
+		try{
+			const result = await bd.query(
+				'UPDATE administracion.ambulancias SET '+
+				'num_placa_a = $2, num_economico_a=$3, estado_a=$4 '+
+				'WHERE id_a=$1 AND id_cm=$5',
+				[req.body['id_a'],req.body['num_placa_a'],req.body['num_economico_a'],req.body['estado_a'],req.user.id_cm]	
+			)
+			res.redirect('/crum/unidades')
+		}
+		catch(err){
+			console.log(err)
+		}
+	},	
 	crumPanelUsuarios: (req, res) => {
 		bd.query({
 			text: 'SELECT * FROM perfiles.pacientes WHERE id_cm = $1',
@@ -222,7 +224,7 @@ module.exports = {
 			text: 'DELETE FROM ...',
 			values: [req.body['id_usr'],req.user.id_cm]
 		}, (err,result) => {
-
+			
 		})
 	},
 	crumPanelUsuarios_C: (req,res) => {
@@ -230,8 +232,9 @@ module.exports = {
 			text: 'UPDATE ...',
 			values: [req.body['id_usr'],req.user.id_cm]
 		}, (err,result) => {
-
+			
 		})
 	}
 }
-
+	
+	
