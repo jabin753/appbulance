8349f5e66128d599f47a658aa6e95ec7f2f22853
jabin753 <jabin753@gmail.com>
2018-06-13 22:49:50 -0500

    Removed body-parser and  google-maps node API (Redundant Snippets)

--- src/config/app.js
+++ src/config/app.js
@@ -1,28 +1,28 @@
 var morgan = require('morgan')
 var path = require('path')
 var express = require('express')
-var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var passport = require('passport')
     require('./passport')(passport)
 var flash = require('connect-flash')
 var BDPrueba = require('./dbConnection').prueba()
-
 var session = require('express-session')
+
 var app = express()
 
-app.use(bodyParser.json())//Cuerpo de petición y respuesta en formato json
-app.use(bodyParser.urlencoded({extended: false}))//Codificación en la url omitida
-app.use(cookieParser())
-
-
-app.use(express.static(path.join(__dirname,'..','public')))//Ruta de archivos estáticos
-//app.set(express.static('index',false)) //Omite el archivo index.html por defecto
+//Configuraciones
 
 app.set('port',process.env.port || 8888);// Puerto de operación de la aplicación
+app.set('view engine','ejs') //Configuración del motor de vistas
+app.set('views', path.join(__dirname, '../views')) //Directorio donde están las vistas
 
+//Middleware
+
+app.use(express.json())//Cuerpo de petición y respuesta en formato json
+app.use(express.urlencoded({extended: false}))//Codificación en la url omitida
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'..','public')))//Ruta de archivos estáticos
 app.use(morgan('tiny'))
-
 app.use(session({//Sesión secreta. Requerida por passport
     secret: '::appbulance2018::',
     cookie:{maxAge:900000000000},
@@ -32,23 +32,10 @@ app.use(session({//Sesión secreta. Requerida por passport
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session()) //Logins persistentes
-//Gestión de rutas
+
+//Rutas
+
 require('../routes/routes')(app,passport)
 
-app.set('view engine','ejs') //Configuración del motor de vistas
-app.set('views', path.join(__dirname, '../views')) //Directorio donde están las vistas
 
-/*google maps API
-var googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyDa56T84jpoLhurxSlOf9lou21Xj0jAl90'
-  });
-  googleMapsClient.g
-
-  googleMapsClient.geocode({
-    address: 'privada javier mina 67'}, function(err, response) {
-    if (!err) {
-      console.log(response.json.results)
-    }
-  })
-  */
 module.exports = app
