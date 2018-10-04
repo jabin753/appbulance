356ed1eaab4a23f054c97f8a0a794ebfa1723f55
Joseph Jabin <jabin753@gmail.com>
2018-10-04 08:45:44 -0500

    PWA enabled

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,5 +1,7 @@
-var CACHE_NAME = 'appbulance-cache-v1';
+var CACHE_NAME = 'appbulance-cache-v1.2';
 var urlsToCache = [
+  '/',
+  '/images/logo.png',
   '/vendor/bootstrap/css/bootstrap.min.css'
 ];
 
@@ -12,4 +14,45 @@ self.addEventListener('install', function(event) {
         return cache.addAll(urlsToCache);
       })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          console.log('Devolviendo '+response.url);
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
