f3d1bddf56a0feb0a51a9249f48e320826090e30
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-11-06 01:57:59 -0600

    Agregar las rutas de las páginas de FAQ y Web version

--- src/controllers/inicio.js
+++ src/controllers/inicio.js
@@ -1,6 +1,14 @@
 module.exports = {
   inicio: (req,res)=>{
     res.render('index-appbulance',
-    {pageTitle:'Appbulance - Bienvenido'})
+    {pageTitle:'Appbulance - Sitio Oficial'})
+  },
+  faq: (req,res)=>{
+    res.render('index-appbulance-faq',
+    {pageTitle:'Appbulance - Preguntas frecuentes'})
+  },
+  web_version: (req,res)=>{
+    res.render('index-appbulance-web',
+    {pageTitle:'Appbulance - Version web'})
   }
 }
