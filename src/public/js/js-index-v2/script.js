310ef6dc862cee7793db42f153a06b903092d211
Jose Luis Alcantara Bustamante <luizalcantara59@gmail.com>
2018-11-10 14:16:29 -0600

    Actualizacion del sitio - Estabilizacion

--- src/public/js/js-index-v2/script.js
+++ src/public/js/js-index-v2/script.js
@@ -9,6 +9,18 @@ Copyright © All rights Reserved
 
 */
 
+/*if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }*/
+
 $(function() {
     "use strict";
 
