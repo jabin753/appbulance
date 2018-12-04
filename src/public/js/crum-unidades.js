cf990b0aef51b15fcf1d31523fbf26a6ea478526
jabin753 <jabin753@gmail.com>
2018-12-04 14:02:47 -0600

    Ambulancias event methods changed

--- src/public/js/crum-unidades.js
+++ src/public/js/crum-unidades.js
@@ -1,10 +1,8 @@
-var ambulancias = {};
-var ambulancia = {};
-
+var ambulancia;
+var tableAmbulancias;
 $(document).ready(function () {
-    $.getJSON("/api/admin/a", function(json){
-        console.log(tableDefaultConfig);
-        ambulancias = $("#ambulancias").DataTable(Object.assign(tableDefaultConfig,
+    $.getJSON("/api/admin/a", function (json) {
+        tableAmbulancias = $("#ambulancias").DataTable(Object.assign(tableDefaultConfig,
             {
                 data: json,
                 rowId: 'id_a',
@@ -14,37 +12,37 @@ $(document).ready(function () {
                     { data: 'estado_a' }
                 ]
             }));
-    });
-
-    //DOM EVENTS
-
-    $(document).on('click','#ambulancias tr',function(){
-        ambulancia = ambulancias.row(this).data();
-        if(typeof ambulancia !== 'undefined'){
+        //DOM EVENTS
+        tableAmbulancias.on('select',function (e, dt, type, indexes) {
             $("button[name='btnDel']").attr("disabled", false);
             $("button[name='btnMod']").attr("disabled", false);
-        }else{
+            ambulancia = tableAmbulancias.row(indexes).data();
+            console.log(ambulancia);
+        });
+        tableAmbulancias.on('deselect', function(e, dt, type, indexes) {
             $("button[name='btnDel']").attr("disabled", true);
             $("button[name='btnMod']").attr("disabled", true);
-        }
+            ambulancia = {};
+        });
     });
-    $("button[name='btnMod']").on("click", function(){ 
+
+    $("button[name='btnMod']").on("click", function () {
         var $form = $('#FRMput_a');
-        $form.find("input[name='INnum_placa_a']").attr("value",ambulancia.num_placa_a);
-        $form.find("input[name='INnum_economico_a']").attr("value",ambulancia.num_economico_a);
-        $form.find("input[name='INestado_a']").attr("value",ambulancia.estado_a);
+        $form.find("input[name='INnum_placa_a']").attr("value", ambulancia.num_placa_a);
+        $form.find("input[name='INnum_economico_a']").attr("value", ambulancia.num_economico_a);
+        $form.find("input[name='INestado_a']").attr("value", ambulancia.estado_a);
     });
-    $('#FRMpost_a').on('submit',function(e){
+    $('#FRMpost_a').on('submit', function (e) {
         e.preventDefault();
         post_a();
         reload();
     });
-    $('#FRMdelete_a').on('submit',function(e){
+    $('#FRMdelete_a').on('submit', function (e) {
         e.preventDefault();
         delete_a();
         reload();
     });
-    $('#FRMput_a').on('submit',function(e){
+    $('#FRMput_a').on('submit', function (e) {
         e.preventDefault();
         put_a();
         reload();
@@ -53,53 +51,53 @@ $(document).ready(function () {
 
 //FUNCTIONS
 
-function post_a(){
+function post_a() {
     $.ajax({
         url: '/api/admin/a',
         method: 'POST',
         data: {
             num_placa_a: $('#INnum_placa_a').val(),
             num_economico_a: $('#INnum_economico_a').val(),
-            estado_a: parseInt($('#INestado_a').val()) 
+            estado_a: parseInt($('#INestado_a').val())
         },
-        success: function(response){
+        success: function (response) {
             console.log(response);
         },
-        error: function(err){
+        error: function (err) {
             console.log(err);
         }
     });
 }
-function put_a(){
+function put_a() {
     var $form = $('#FRMput_a');
     $.ajax({
-        url: '/api/admin/a/'+ ambulancia.id_a,
+        url: '/api/admin/a/' + ambulancia.id_a,
         method: 'PUT',
-        data:{
+        data: {
             num_placa_a: $form.find("input[name='INnum_placa_a']").val(),
             num_economico_a: $form.find("input[name='INnum_economico_a']").val(),
             estado_a: parseInt($form.find("input[name='INestado_a']").val())
         },
-        success: function(response){
+        success: function (response) {
             console.log(response);
         },
-        error: function(err){
+        error: function (err) {
             console.log(err);
         }
     });
 }
-function delete_a(){
+function delete_a() {
     $.ajax({
-        url: '/api/admin/a/'+ ambulancia.id_a,
+        url: '/api/admin/a/' + ambulancia.id_a,
         method: 'DELETE',
-        success: function(response){
+        success: function (response) {
             console.log(response);
         },
-        error: function(err){
+        error: function (err) {
             console.log(err);
         }
     });
 }
 function reload() {
-	location.reload();
+    location.reload();
 }
