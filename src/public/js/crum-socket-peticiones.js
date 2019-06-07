9a4d48a8126d5ee8fc9f0887c618d43f567efb84
Alcantarex <luizalcantara59@gmail.com>
2019-06-06 19:44:09 -0500

    Add new functions to petition modal

--- src/public/js/crum-socket-peticiones.js
+++ src/public/js/crum-socket-peticiones.js
@@ -12,7 +12,7 @@ socket.on('receivePetition',function(data){
         draggable: false
     });
     map.setCenter(pos);
-    map.setZoom(17);
+    map.setZoom(17);    
 
     if(Push.Permission.has()) {
         Push.create("Petición entrante!", {
@@ -26,20 +26,77 @@ socket.on('receivePetition',function(data){
         });
     }
 
+    function sexo(s){
+        if(s=="M")
+            return "Hombre";
+        else 
+            return "Mujer";
+    }
+
+    function calcularEdad(fecha) {
+        fecha = fecha.substring(0,10);
+        var hoy = new Date();
+        var cumpleanos = new Date(fecha);
+        var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+        var m = hoy.getMonth() - cumpleanos.getMonth();    
+        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+            edad--;
+        }    
+        return edad;
+    }
+
     $('#modal-body-petition').html(function(){
-        return "<h2>Datos del paciente</h2>\
+        return "<h3>Datos del paciente</h3>\
         <hr>\
-        <p>Usuario:</p>\
-        <p id=\"modal_id_p\">"+data.nombre_prs+"</p>\
-        <hr>\
-        <h2>Ubicación</h2>\
-        <hr>\
-        <p>Dirección:</p>\
-        <p id=\"modal_direccion_pt\">"+data.direccion_pt+"</p>\
-        <p>Latitud:</p>\
-        <p id=\"modal_ubicacion_pt_x\">"+data.ubicacion_pt.x+"</p>\
-        <p>Longitud:</p>\
-        <p id=\"modal_ubicacion_pt_y\">"+data.ubicacion_pt.y+"</p>"
+        <table class=\"table\">\
+            <tbody>\
+                <tr>\
+                    <th scope=\"row\">Nombre:</th>\
+                    <td>"+ data.nombre_prs + "</td>\
+                </tr>\
+                <tr>\
+                    <th scope=\"row\">Edad:</th>\
+                    <td>"+ calcularEdad(data.fecha_nacimiento_prs) + " años</td>\
+                </tr>\
+                <tr>\
+                    <th scope=\"row\">Teléfono:</th>\
+                    <td>"+ data.telefono_usr + "</td>\
+                </tr>\
+                <tr>\
+                    <th scope=\"row\">Sexo:</th>\
+                    <td>"+ sexo(data.sexo_prs) + "</td>\
+                </tr>\
+                <tr>\
+                    <th scope=\"row\">Tipo de Sangre:</th>\
+                    <td>"+ data.tipo_sangre_p + "</td>\
+                </tr>\
+                <tr>\
+                    <th scope=\"row\">NSS:</th>\
+                    <td>"+ data.nss_p + "</td>\
+                </tr>\
+                <tr>\
+                    <th scope=\"row\">Dirección:</th>\
+                    <td>"+ data.direccion_pt + "</td>\
+                </tr>\
+            </tbody>\
+        </table>"        
     });
-    $('#incoming_petition').modal('show');
+    $('#incoming_petition').modal('show');   
+    playSound();
+    
+    
 });
+
+// Función de reproducción de notificación
+function playSound(){
+	$(".audio")[0].play();
+}
+
+/*
+<p>Dirección:</p>\
+<p id=\"modal_direccion_pt\">"+data.direccion_pt+"</p>\
+<p>Latitud:</p>\
+<p id=\"modal_ubicacion_pt_x\">"+data.ubicacion_pt.x+"</p>\
+<p>Longitud:</p>\
+<p id=\"modal_ubicacion_pt_y\">"+data.ubicacion_pt.y+"</p>"
+*/
